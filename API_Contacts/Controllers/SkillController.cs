893064a55a19d05368e7031d6e8ba0e543138fda
michael.n.descamps <michael.n.descamps@gmail.com>
2020-09-19 22:02:32 +0200

    Implemented Skill controller with LINQ queries

--- API_Contacts/Controllers/SkillController.cs
+++ API_Contacts/Controllers/SkillController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using API_Contacts.DataAccess;
+using API_Contacts.ViewModels;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
@@ -13,30 +14,74 @@ namespace API_Contacts.Controllers
     [ApiController]
     public class SkillController : ControllerBase
     {
+        private readonly IRepository<Contact> _contactRepository;
+        private readonly IRepository<ContactSkill> _contactskillRepository;
         private readonly IRepository<Skill> _skillRepository;
 
-        public SkillController(IRepository<Skill> skillRepository)
+        public SkillController(
+            IRepository<Contact> contactRepository,
+            IRepository<ContactSkill> contactskillRepository,
+            IRepository<Skill> skillRepository
+            )
         {
+            _contactRepository = contactRepository;
+            _contactskillRepository = contactskillRepository;
             _skillRepository = skillRepository;
         }
 
         [HttpGet]
-        public IEnumerable<Skill> Get()
+        public IActionResult Get()
         {
-            return _skillRepository.GetAll();
+            var list_skills = new List<SkillViewModel> { };
+
+            foreach (Skill s in _skillRepository.GetAll())
+            {
+
+                var queryContacts = (from contacts in _contactRepository.GetAll()
+                                   join contactskills in _contactskillRepository.GetAll()
+                                   on contacts.Id equals contactskills.IdContact
+                                   where contactskills.IdSkill == s.Id
+                                   select String.Concat(contacts.FirstName," ", contacts.LastName)).ToList();
+
+                list_skills.Add(new SkillViewModel
+                {
+                    Id = s.Id,
+                    SkillName=s.SkillName,
+                    SkillLevel = s.SkillLevel,
+                    FullNames = queryContacts
+                });
+
+            }
+            return Ok(list_skills);
         }
 
         //GET Skill/5
         [HttpGet("{id}", Name = "GetSkill")]
         public IActionResult Get(int id)
         {
-            var skill = _skillRepository.GetById(id);
-            if (skill == null)
+
+            var skillRepo = _skillRepository.GetById(id);
+            if (skillRepo == null)
             {
                 return NotFound();
             }
 
-            return Ok(skill);
+            var queryContacts = (from contacts in _contactRepository.GetAll()
+                                 join contactskills in _contactskillRepository.GetAll()
+                                 on contacts.Id equals contactskills.IdContact
+                                 where contactskills.IdSkill == skillRepo.Id
+                                 select String.Concat(contacts.FirstName, " ", contacts.LastName)).ToList();
+
+            var skillDisplay = new SkillViewModel
+            {
+                Id = skillRepo.Id,
+                SkillName = skillRepo.SkillName,
+                SkillLevel = skillRepo.SkillLevel,
+                FullNames = queryContacts
+            };
+
+            return Ok(skillDisplay);
+
         }
 
         //post Contact
@@ -54,19 +99,12 @@ namespace API_Contacts.Controllers
         }
         //TODO fix the put method
         //put Skill/5
-        [HttpPost("{id}")]
+        [HttpPut("{id}")]
         public IActionResult Put(int id, [FromBody] Skill value)
         {
             if (value == null)
             {
                 return BadRequest();
-            }
-
-            var note = _skillRepository.GetById(id);
-
-            if (note == null)
-            {
-                return NotFound();
             }
 
             value.Id = id;
---
+++ API_Contacts/ViewModels/SkillViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API_Contacts.ViewModels
+{
+    public class SkillViewModel
+    {
+        public int Id { get; set; }
+        public string SkillName { get; set; }
+        public string SkillLevel { get; set; }
+
+        public List<string> FullNames { get; set; }
+    }
+}
