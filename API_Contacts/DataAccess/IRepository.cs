fa1f8d2de253c6b095a48888b308561d5b2796fc
michael.n.descamps <michael.n.descamps@gmail.com>
2020-09-19 14:16:39 +0200

    Data access and controller Repository and associated interface Added controller Changed the connection string to SQL server

---
+++ API_Contacts/DataAccess/IRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API_Contacts.DataAccess
+{
+    public interface IRepository<T> where T : class
+    {
+        T Add(T item);
+        IEnumerable<T> GetAll();
+        T GetById(int id);
+        void Delete(T item);
+        void Update(T item);
+
+    }
+}
---
+++ API_Contacts/DataAccess/InMemoryRepository.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API_Contacts.DataAccess
+{
+    public class InMemoryRepository<T> : IRepository<T> where T : class
+    {
+        private readonly DBContactsContext _context;
+
+        public InMemoryRepository(DBContactsContext context)
+        {
+            _context = context;
+        }
+        public T Add(T item)
+        {
+            try
+            {
+                T added = (_context.Set<T>().Add(item)).Entity;
+                _context.SaveChanges();
+                return added;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        public void Delete(T item)
+        {
+            _context.Set<T>().Remove(item);
+            _context.SaveChanges();
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            return _context.Set<T>().ToList();
+        }
+
+        public T GetById(int id)
+        {
+            return _context.Set<T>().Find(id);
+        }
+
+        public void Update(T item)
+        {
+            _context.Attach(item);
+            _context.Entry(item).State = EntityState.Modified;
+            _context.SaveChanges();
+        }
+    }
+}
