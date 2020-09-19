400745f486f45527b9e8fed76c0af5d2a981468b
michael.n.descamps <michael.n.descamps@gmail.com>
2020-09-19 14:19:05 +0200

    Implement contact controller enable allowanonymous and disable windows authentication

--- API_Contacts/Controllers/ContactController.cs
+++ API_Contacts/Controllers/ContactController.cs
@@ -2,15 +2,92 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using API_Contacts.DataAccess;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace API_Contacts.Controllers
 {
-    public class ContactController : Controller
+    [AllowAnonymous]
+    [Route("[controller]")]
+    [ApiController]
+    public class ContactController : ControllerBase
     {
-        public IActionResult Index()
+        private readonly IRepository<Contact> _contactRepository;
+
+        public ContactController(IRepository<Contact> contactRepository)
         {
-            return View();
+            _contactRepository = contactRepository;
         }
+
+        [HttpGet]
+        public IEnumerable<Contact> Get()
+        {
+            return _contactRepository.GetAll();
+        }
+
+        //GET Contact/5
+        [HttpGet("{id}", Name = "GetContact")]
+        public IActionResult Get(int id)
+        {
+            var contact = _contactRepository.GetById(id);
+            if (contact == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(contact);
+        }
+
+        //post Contact
+        [HttpPost]
+        public IActionResult Post([FromBody] Contact value)
+        {
+            if (value == null)
+            {
+                return BadRequest();
+            }
+            var createdContact = _contactRepository.Add(value);
+
+            return CreatedAtAction("Get", new { id = createdContact.Id, createdContact });
+
+        }
+        //TODO fix the put method
+        //put Contact/5
+        [HttpPost("{id}")]
+        public IActionResult Put(int id, [FromBody] Contact value)
+        {
+            if (value == null)
+            {
+                return BadRequest();
+            }
+
+            var note = _contactRepository.GetById(id);
+
+            if (note == null)
+            {
+                return NotFound();
+            }
+
+            value.Id = id;
+            _contactRepository.Update(value);
+            return NoContent();
+        }
+
+        //delete Contact/5
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            var contact = _contactRepository.GetById(id);
+            if (contact == null)
+            {
+                return NotFound();
+            }
+
+            _contactRepository.Delete(contact);
+
+            return NoContent();
+        }
+
     }
 }
